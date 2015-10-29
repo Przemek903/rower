42f473384009bda647031aaebd0eda5b3d9ce47a
Przemek903 <przemek903@gazeta.pl>
2015-10-29 15:35:29 +0100

    fixed conflict in config.ru

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rower::Application.config.secret_key_base = 'ed30f645a1815fa1f94fbabd2b2318b71e3f7ee4578ec300eb719511f1ce3a585a13f4fcc5aa3eab40d99ec07cce41b721c2cdf80b94288d3f0f3421ae476b25'
